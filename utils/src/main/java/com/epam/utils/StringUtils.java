882b07d38f4fd9a2812fab30f1245758f99f35a4
Hanna_Urban <huan92@yandex.ru>
2024-03-15 14:51:46 +0300

    done

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            Double number = Double.parseDouble(str);
+            if (number > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        catch (NumberFormatException e)
+        {return false;}
     }
 }
